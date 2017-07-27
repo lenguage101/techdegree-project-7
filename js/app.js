cebf495bacdebd0a8b46eac99314711410d50491
Tommy Leng <tommyleng@yahoo.com>
2017-07-27 13:25:31 -0700

    Add mediaelement.js and more media queries

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 let vid = document.querySelector('video');
-let span = document.getElementsByTagName("span");
+let span = $(".content span");
 
 vid.volume =.5;
+// $('video').mediaelementplayer({
+//   features: ['playpause', 'tracks', 'progress'],
+//   startLanguage: 'en'
+// });
 vid.addEventListener('timeupdate', function() {
 	let now = vid.currentTime;
 	// highlight text as video plays
 	for (let i = 0; i < span.length; i++) {
-		console.log(now*2);
 	  if (now >= span[i].getAttribute("data-start") &&
 	      now <= span[i].getAttribute("data-end")) {
 	    span[i].className = "highlight";
